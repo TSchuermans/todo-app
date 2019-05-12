7a02b8b635894575ba89a234e893b1c48e389bb7
Tom Schuermans <tom@nucleus.be>
2019-05-12 23:04:45 +0200

    Symfony: installed twig & sensio framework extra bundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
